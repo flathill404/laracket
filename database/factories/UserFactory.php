e0b66f80fce8a737ea2753603e331ee84bad145e
flathill404 <38638577+flathill404@users.noreply.github.com>
2026-01-05 09:15:53 +0000

    add organization relationship of user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -11,34 +11,30 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
+            'organization_id' => \App\Models\Organization::factory(),
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
+            'is_representative' => false,
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    public function representative(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_representative' => true,
         ]);
     }
 }
