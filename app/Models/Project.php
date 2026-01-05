b159f5575efc36c30ebb4b80c5dda06dcb86cadb
flathill404 <38638577+flathill404@users.noreply.github.com>
2026-01-05 09:08:20 +0000

    add project relations

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function organization()
+    {
+        return $this->belongsTo(Organization::class);
+    }
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
+    public function assignedTeams()
+    {
+        return $this->morphToMany(Team::class, 'assignable', 'project_assignments')
+                    ->withTimestamps();
+    }
+
+    public function assignedUsers()
+    {
+        return $this->morphToMany(User::class, 'assignable', 'project_assignments')
+                    ->withTimestamps();
+    }
 }
