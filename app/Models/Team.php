19c2631aeb898b83eec746f5cc04d9f9e018cfcd
flathill404 <38638577+flathill404@users.noreply.github.com>
2026-01-05 09:06:25 +0000

    add team relations

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function organization()
+    {
+        return $this->belongsTo(Organization::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class)
+                    ->withPivot('role')
+                    ->withTimestamps();
+    }
+
+    public function assignedProjects()
+    {
+        return $this->morphedByMany(Project::class, 'assignable', 'project_assignments')
+                    ->withTimestamps();
+    }
 }
