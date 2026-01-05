73d37872bb6fe9f0c7d85cc6723986b6fc1005e5
flathill404 <38638577+flathill404@users.noreply.github.com>
2026-01-05 09:08:35 +0000

    add ticket relations

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    protected $guarded = [];
+
+    protected $casts = [
+        'deadline' => 'datetime',
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function assignees()
+    {
+        return $this->belongsToMany(User::class, 'ticket_user')
+                    ->wherePivot('role', 'assignee')
+                    ->withTimestamps();
+    }
+
+    public function reviewers()
+    {
+        return $this->belongsToMany(User::class, 'ticket_user')
+                    ->wherePivot('role', 'reviewer')
+                    ->withTimestamps();
+    }
 }
