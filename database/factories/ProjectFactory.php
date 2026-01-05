e94953e428fa339b0bef00f7f9e20be6b63ff74d
flathill404 <38638577+flathill404@users.noreply.github.com>
2026-01-05 09:17:13 +0000

    add project factory

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->catchPhrase() . ' Project',
+            'summary' => $this->faker->realText(50),
+            'deadline' => $this->faker->dateTimeBetween('now', '+1 year'),
+            'organization_id' => \App\Models\Organization::factory(),
+        ];
+    }
+}
