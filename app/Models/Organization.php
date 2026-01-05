a63225694ad347771edf81121495c63bb00892a4
flathill404 <38638577+flathill404@users.noreply.github.com>
2026-01-05 09:05:42 +0000

    add organization relations

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Organization extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function teams()
+    {
+        return $this->hasMany(Team::class);
+    }
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
 }
