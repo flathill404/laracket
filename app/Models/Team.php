d71f8c1a55eb4b4c2710195fcaa201fc1846c8d7
flathill404 <38638577+flathill404@users.noreply.github.com>
2026-01-06 04:31:33 +0000

    add team typehints

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -4,26 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Team extends Model
 {
+    /** 
+     * @use HasFactory<\Database\Factories\TeamFactory> 
+     */
     use HasFactory;
 
     protected $guarded = [];
 
-    public function organization()
+    /**
+     * @return BelongsTo<Organization, $this>
+     */
+    public function organization(): BelongsTo
     {
         return $this->belongsTo(Organization::class);
     }
 
-    public function users()
+    /**
+     * @return BelongsToMany<User, $this>
+     */
+    public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class)
             ->withPivot('role')
             ->withTimestamps();
     }
 
-    public function assignedProjects()
+    /**
+     * @return MorphToMany<Project, $this>
+     */
+    public function assignedProjects(): MorphToMany
     {
         return $this->morphToMany(Project::class, 'assignable', 'project_assignments')
             ->withTimestamps();
