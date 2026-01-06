11b29025b37441bb3544ac6ab90db841d10cdcb3
flathill404 <38638577+flathill404@users.noreply.github.com>
2026-01-06 04:29:41 +0000

    add project typehints

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,30 +4,48 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Project extends Model
 {
+    /** 
+     * @use HasFactory<\Database\Factories\ProjectFactory> 
+     */
     use HasFactory;
 
     protected $guarded = [];
 
-    public function organization()
+    /**
+     * @return BelongsTo<Organization, $this>
+     */
+    public function organization(): BelongsTo
     {
         return $this->belongsTo(Organization::class);
     }
 
-    public function tickets()
+    /**
+     * @return HasMany<Ticket, $this>
+     */
+    public function tickets(): HasMany
     {
         return $this->hasMany(Ticket::class);
     }
 
-    public function assignedTeams()
+    /**
+     * @return MorphToMany<Team, $this>
+     */
+    public function assignedTeams(): MorphToMany
     {
         return $this->morphedByMany(Team::class, 'assignable', 'project_assignments')
             ->withTimestamps();
     }
 
-    public function assignedUsers()
+    /**
+     * @return MorphToMany<User, $this>
+     */
+    public function assignedUsers(): MorphToMany
     {
         return $this->morphedByMany(User::class, 'assignable', 'project_assignments')
             ->withTimestamps();
