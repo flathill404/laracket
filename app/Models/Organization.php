15b51838d4752bf00732bf83907c2d300d7021c4
flathill404 <38638577+flathill404@users.noreply.github.com>
2026-01-06 03:31:42 +0000

    add organization typehints

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -3,25 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 
 class Organization extends Model
 {
+    /**
+     * @use HasFactory<\Database\Factories\OrganizationFactory>
+     */
     use HasFactory;
 
     protected $guarded = [];
 
-    public function users()
+    /**
+     * @return HasMany<User, $this>
+     */
+    public function users(): HasMany
     {
         return $this->hasMany(User::class);
     }
 
-    public function teams()
+    /**
+     * @return HasMany<Team, $this>
+     */
+    public function teams(): HasMany
     {
         return $this->hasMany(Team::class);
     }
 
-    public function projects()
+    /**
+     * @return HasMany<Project, $this>
+     */
+    public function projects(): HasMany
     {
         return $this->hasMany(Project::class);
     }
