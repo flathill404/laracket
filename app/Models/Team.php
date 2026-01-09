0350ab524cc90f9e7947acaf0fbeed33b032001f
flathill404 <38638577+flathill404@users.noreply.github.com>
2026-01-09 03:20:44 +0000

    lint

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-
 /**
  * @property string $id
  * @property string $organization_id
