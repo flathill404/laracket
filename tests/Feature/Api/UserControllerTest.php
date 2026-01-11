91721352e8f849e0775cc21275ae50366ad3a423
flathill404 <38638577+flathill404@users.noreply.github.com>
2026-01-11 06:12:53 +0000

    user test

---
+++ tests/Feature/Api/UserControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\getJson;
+
+uses(LazilyRefreshDatabase::class);
+
+describe('show', function () {
+    it('returns current user profile', function () {
+        $user = User::factory()->create();
+        actingAs($user);
+
+        getJson('/api/user')
+            ->assertOk()
+            ->assertJson([
+                'id' => $user->id,
+                'email' => $user->email,
+            ]);
+    });
+
+    it('denies access if not authenticated', function () {
+        getJson('/api/user')
+            ->assertUnauthorized();
+    });
+});
