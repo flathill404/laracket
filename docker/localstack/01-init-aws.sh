5927808d1fb8d4fc55eaf08c72935a3afaa8739e
flathill404 <38638577+flathill404@users.noreply.github.com>
2026-01-19 12:53:57 +0900

    feat: configure public bucket policy and CORS for the local S3 bucket

--- docker/localstack/01-init-aws.sh
+++ docker/localstack/01-init-aws.sh
@@ -3,5 +3,35 @@
 # Create S3 bucket
 awslocal s3 mb s3://local-bucket
 
+# Set public bucket policy
+awslocal s3api put-bucket-policy --bucket local-bucket --policy '{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadWrite",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:*",
+      "Resource": [
+        "arn:aws:s3:::local-bucket",
+        "arn:aws:s3:::local-bucket/*"
+      ]
+    }
+  ]
+}'
+
+# Set CORS configuration
+awslocal s3api put-bucket-cors --bucket local-bucket --cors-configuration '{
+  "CORSRules": [
+    {
+      "AllowedHeaders": ["*"],
+      "AllowedMethods": ["GET", "PUT", "POST", "DELETE", "HEAD"],
+      "AllowedOrigins": ["*"],
+      "ExposeHeaders": ["ETag"],
+      "MaxAgeSeconds": 3000
+    }
+  ]
+}'
+
 # Verify SES identity
 awslocal ses verify-email-identity --email-address hello@example.com
